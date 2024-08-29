d882e5d474386d5e90d708c3d9e3398c2a997fe2
超级无敌大帅比 <cerqt2486@gmail.com>
2024-08-30 06:30:30 +0800

    Update Bilibili_Video_Progress_Tool.js

--- Bilibili_Video_Progress_Tool.js
+++ Bilibili_Video_Progress_Tool.js
@@ -8,6 +8,7 @@
 // @match        https://www.bilibili.com/video/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function() {
